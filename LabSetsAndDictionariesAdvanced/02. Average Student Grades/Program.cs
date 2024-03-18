564c624e9a5ac3254c567dfcff8485ac4c6c5694
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-18 23:05:25 +0200

    Update Program.cs

--- LabSetsAndDictionariesAdvanced/02. Average Student Grades/Program.cs
+++ LabSetsAndDictionariesAdvanced/02. Average Student Grades/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+
+                string name = input[0];
+                decimal grade = decimal.Parse(input[1]);
+
+                if (!students.ContainsKey(name))
+                {
+                        students[name] = new List<decimal>();
+                }
+                students[name].Add(grade);
+            }
+
+            foreach (var student in students)
+            {
+                Console.Write($"{student.Key} -> ");
+
+                foreach (var grade in student.Value)
+                {
+                    Console.Write($"{grade:F2} ");
+                }
+                Console.WriteLine($"(avg: {student.Value.Average():F2})");
+            }
         }
     }
 }
