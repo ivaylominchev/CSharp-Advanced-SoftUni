8ba45dc241d586cda3b1904b6d7b1a2daffc8ef8
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-18 22:16:18 +0200

    Update Program.cs

--- LabSetsAndDictionariesAdvanced/01. Count Same Values in Array/Program.cs
+++ LabSetsAndDictionariesAdvanced/01. Count Same Values in Array/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace _01._Count_Same_Values_in_Array
+﻿using System.ComponentModel;
+
+namespace _01._Count_Same_Values_in_Array
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> dict = new Dictionary<double, int>();
+
+            foreach (double number in numbers)
+            {
+                if (!dict.ContainsKey(number))
+                {
+                    dict[number] = 0;
+                }
+                dict[number]++;
+            }
+            foreach (var pair in dict)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value} times");
+            }
         }
     }
 }
