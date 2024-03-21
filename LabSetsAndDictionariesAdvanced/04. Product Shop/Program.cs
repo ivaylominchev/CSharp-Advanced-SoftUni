03bd819015308aadd9d300902561bd636ec0b792
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-21 23:20:30 +0200

    Update Program.cs

--- LabSetsAndDictionariesAdvanced/04. Product Shop/Program.cs
+++ LabSetsAndDictionariesAdvanced/04. Product Shop/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var shops = new Dictionary<string, Dictionary<string, double>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Revision")
+            {
+                var tokens = input
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string shopName = tokens[0];
+                string product = tokens[1];
+                double price = double.Parse(tokens[2]);
+
+                if (!shops.ContainsKey(shopName))
+                {
+                    shops.Add(shopName, new Dictionary<string, double>());
+                }
+
+                if (!shops[shopName].Keys.Contains(product))
+                {
+                    shops[shopName].Add(product, price);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var shop in shops.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{shop.Key}->");
+
+                foreach (var kvp in shop.Value)
+                {
+                    Console.WriteLine($"Product: {kvp.Key}, Price: {kvp.Value}");
+                }
+            }
         }
     }
 }
